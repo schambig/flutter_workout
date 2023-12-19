16edb2e4d051d5a7dab145e67c5884a1e4a3467a
Salomon Chambi <salomonchambi@gmail.com>
2023-12-19 18:41:25 -0500

    feat: add comments for map and set, use keys and values in map

--- dart_fundamentals/01-map.dart
+++ dart_fundamentals/01-map.dart
@@ -1,4 +1,5 @@
 void main() {
+  // This is a map
   // var mascota = {
   final mascota = <String, dynamic>{
     'name': 'Garfilito',
@@ -6,9 +7,13 @@ void main() {
     'canReproduce': false,
   };
 
+  // This is a set
   final mascotas = <String>{'Pequeña', 'Blanca', 'Colitas', 'Manchas', 'Pequeña'};
 
   print(mascota.runtimeType);  // _Map<String, Object>
+  print(mascota['name']);  // Garfilito
   print(mascota);  // {name: Garfilito, edad: 2, canReproduce: false}
+  print(mascota.keys);  // return all the keys
+  print(mascota.values);  // return all the values
   print(mascotas);  // {Pequeña, Blanca, Colitas, Manchas}
 }
