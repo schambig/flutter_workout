7932f2064f194fc53765eb9a1082be5ed9f450d3
Salomon Chambi <salomonchambi@gmail.com>
2023-12-19 18:28:13 -0500

    feat: use final keyword and return type value

--- dart_fundamentals/01-map.dart
+++ dart_fundamentals/01-map.dart
@@ -1,9 +1,14 @@
 void main() {
-  var mascota = {
+  // var mascota = {
+  final mascota = <String, dynamic>{
     'name': 'Garfilito',
     'edad': 2,
     'canReproduce': false,
   };
+
+  final mascotas = <String>{'Pequeña', 'Blanca', 'Colitas', 'Manchas', 'Pequeña'};
+
   print(mascota.runtimeType);  // _Map<String, Object>
   print(mascota);  // {name: Garfilito, edad: 2, canReproduce: false}
+  print(mascotas);  // {Pequeña, Blanca, Colitas, Manchas}
 }
