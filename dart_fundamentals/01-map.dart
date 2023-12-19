8df49f9dc4b48d344265dcdbccaa710bd8455c15
Salomon Chambi <salomonchambi@gmail.com>
2023-12-19 18:24:18 -0500

    feat: crate a map and print it

---
+++ dart_fundamentals/01-map.dart
@@ -0,0 +1,9 @@
+void main() {
+  var mascota = {
+    'name': 'Garfilito',
+    'edad': 2,
+    'canReproduce': false,
+  };
+  print(mascota.runtimeType);  // _Map<String, Object>
+  print(mascota);  // {name: Garfilito, edad: 2, canReproduce: false}
+}
